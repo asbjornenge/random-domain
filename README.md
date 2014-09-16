a1e3bccdd6c2b2d06bbab8fdaadedcd910214839
Asbjørn Enge <asbjorn@hanafjedle.net>
2014-09-16 10:32:31 +0200

    A few updates

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Returns a random domain using [random-word]()  and [domains]().
 
 ## Install
 
-    npm i random-domain --save
+    npm install random-domain
 
 ## Use
 
--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var domain = require('random-word')
 var tld    = require('domains')
 
 module.exports = function () {
-    return domain()+'.'+tld[Math.floor((Math.random() * tld.length-1) + 1)]
+    return domain()+'.'+tld[Math.floor((Math.random() * tld.length))]
 }
